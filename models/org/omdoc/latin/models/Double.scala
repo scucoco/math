4ab1ff0e621ec69ab69a21e622b840654962c451
Florian Rabe <f.rabe@jacobs-university.de>
2015-10-14 13:14:32 +0200

    no message

--- models/org/omdoc/latin/models/Double.scala
+++ models/org/omdoc/latin/models/Double.scala
@@ -6,17 +6,17 @@ import info.kwarc.mmt.api.uom._
 import info.kwarc.mmt.api.parser._
 
 object IEEEDouble extends Float {
-  val Arithmetic_num = new RealizedType {
+  val Numbers_num = new RealizedType {
      type univ = Double
      def fromString(s: String) = s.toDouble
      override def lex = Some(new NumberLiteralLexer(true,false))
   }
 
-  def Arithmetic_add(x0: Arithmetic_num.univ, x1: Arithmetic_num.univ): Arithmetic_num.univ = x0 + x1
-  def Arithmetic_sub(x0: Arithmetic_num.univ, x1: Arithmetic_num.univ): Arithmetic_num.univ = x0 - x1
-  def Arithmetic_neg(x0: Arithmetic_num.univ): Arithmetic_num.univ = -x0
-  def Arithmetic_mult(x0: Arithmetic_num.univ, x1: Arithmetic_num.univ): Arithmetic_num.univ = x0 * x1
-  def Arithmetic_div(x0: Arithmetic_num.univ, x1: Arithmetic_num.univ): Arithmetic_num.univ  = x0 / x1
-  def Float_NaN: Arithmetic_num.univ = Double.NaN
-  def Float_infinity: Arithmetic_num.univ = Double.PositiveInfinity
+  def Arithmetic_add(x0: Numbers_num.univ, x1: Numbers_num.univ): Numbers_num.univ = x0 + x1
+  def Arithmetic_sub(x0: Numbers_num.univ, x1: Numbers_num.univ): Numbers_num.univ = x0 - x1
+  def Arithmetic_neg(x0: Numbers_num.univ): Numbers_num.univ = -x0
+  def Arithmetic_mult(x0: Numbers_num.univ, x1: Numbers_num.univ): Numbers_num.univ = x0 * x1
+  def Arithmetic_div(x0: Numbers_num.univ, x1: Numbers_num.univ): Numbers_num.univ  = x0 / x1
+  def Float_NaN: Numbers_num.univ = Double.NaN
+  def Float_infinity: Numbers_num.univ = Double.PositiveInfinity
 }
